cf7e6a69e3eb89347cff4ab35e93a2ab616456ef
Sukhwinder <singhdevelops@gmail.com>
2024-10-03 11:16:18 +0100

    building and testing testnet eth

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.0;
 
 contract Transactions {
-    uint256 transactionCounter;
+    uint256 transactionCount;
 
     event Transfer(address from, address reciever, uint amount, string message, uint256 timestamp, string keyword);
 
@@ -18,5 +18,18 @@ contract Transactions {
 
     TransferStruct[] transactions;
 
-    function addToBlockChain() public {}
+    function addToBlockChain(address payable reciever, uint amount, string memory message, string memory keyword) public {
+        transactionCount += 1;
+        transactions.push(TransferStruct(msg.sender, reciever, amount, message, block.timestamp, keyword));
+
+        emit Transfer(msg.sender, reciever, amount, message, block.timestamp, keyword);
+    }
+
+    function getAllTransactions() public view returns (TransferStruct[] memory) {
+        return transactions;
+    }
+
+    function getTransactionCount() public view returns (uint256) {
+        return transactionCount;
+    }
 }
