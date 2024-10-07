587e2879ee038a4845f0ac6a5fb868cbd2c35bf1
Sukhwinder <singhdevelops@gmail.com>
2024-10-07 11:16:36 +0100

    added vite rule

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
-import abi from './Transactions.json';
+import abi from './Transaction.json';
 
-export const contractAddress = process.env.CONTRACT_ADDRESS;
+export const contractAddress = import.meta.env.VITE_CONTRACT_ADDRESS;
 export const contractABI = abi.abi;
